5be9f13811cd165f98aa1e41bf63a8ecc321432a
giaco <giaco14mw@gmail.com>
2025-02-09 11:54:37 +0100

    kakkona

--- eserciziprol.pl
+++ eserciziprol.pl
@@ -9,7 +9,7 @@ fibonacci(N, F) :-
     fibonacci(N1, F1), fibonacci(N2, F2), F is F1 + F2.
 
 
-/*************************************************/
+/* es Media aritmetica */
 media([], 0).
 media(Xs, M) :-
     somma(Xs, S),
@@ -17,8 +17,6 @@ media(Xs, M) :-
     write(S / L),
     M is (S/ L).
     
-
-
 somma([], 0).
 somma([X|Xs], S) :-
     somma(Xs, S1),
@@ -30,3 +28,28 @@ conta([_|Xs], L) :-
     L is L1 + 1.
 /*************************************************/
 
+/* es Scarta anni bisestili da lista di interi non negativi */
+scarta_bisestili([], []).
+scarta_bisestili([A|As], Bs) :-
+    A mod 400 =:= 0,
+    scarta_bisestili(As, Bs).
+scarta_bisestili([A|As], Bs) :-
+    A mod 100 =\= 0,
+    A mod 4 =:= 0,
+    scarta_bisestili(As, Bs).
+scarta_bisestili([A|As], [B|Bs]) :-
+    scarta_bisestili(As, Bs).
+/*************************************************/
+
+/* es Scarta anni bisestili da lista di interi non negativi */
+scarta_multiplo_cinque([], []).
+
+scarta_multiplo_cinque([X|Xs], Ys) :-
+    X mod 5 =:= 0,
+    X mod 2 =\= 0,
+    scarta_multiplo_cinque(Xs, Ys).
+
+scarta_multiplo_cinque([X|Xs], [X|Ys]) :-
+    \+ (X mod 5 =:= 0, X mod 2 =\= 0),
+    scarta_multiplo_cinque(Xs, Ys).
+/*************************************************/
